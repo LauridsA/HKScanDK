e559f3ba16f459816ff290b25535a3065afc72c0
Laurids Andersen <lauridsandersen2013@gmail.com>
2016-11-23 15:18:01 +0100

    adding dba...

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -7,12 +7,12 @@ import dba.DatabaseAccess;
 public class Controller {
 		private DatabaseAccess dba = new DatabaseAccess();
 	public int getSpeed(int i, int j) {
-		dba.getSpeed(I, j);
+		dba.getSpeed(i, j);
 		
 		return 0;
 	}
 	public int getAvgWeight(int before, int now) {
-		ResultSet res = dba.getAvgWeight();
+		ResultSet res = dba.getAvgWeight(before, now);
 		return 0;
 	}
 
