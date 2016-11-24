720e33bdfedfe26603cbe9f2bfe89c9bbba91f74
Skyggfisk <mahasi93@hotmail.com>
2016-11-24 10:25:16 +0100

    Some UI stuff for production stop. Does it need its own controller?

---
+++ src/ui/ProductionStopController.java
@@ -0,0 +1,25 @@
+package ui;
+
+import controller.Controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+/**
+ * NOT SURE ABOUT THIS.
+ * HALP!
+ */
+
+public class ProductionStopController {
+	
+	Controller ctr = new Controller();
+	
+	@FXML
+	private Label timeField;
+	
+	@FXML
+	private Label lengthField;
+	
+	@FXML
+	private Label descField;
+
+}
