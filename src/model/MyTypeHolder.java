546ae1520764068ff32e1587c2c569167d181b6b
mrurb <minikaliffen@gmail.com>
2016-11-24 12:34:09 +0100

    adfasdf

--- src/model/MyTypeHolder.java
+++ src/model/MyTypeHolder.java
@@ -1,5 +1,11 @@
 package model;
 
+/**
+ * MyTypeHolder is used for returning values to the UI.
+ * When using one of the setting methods or constructors the class will also set what type it is.
+ * 
+ *
+ */
 public class MyTypeHolder {
 	private Integer integer;
 	private String string;
@@ -7,34 +13,66 @@ public class MyTypeHolder {
 	private FieldType type;
 	
 	
+	/**
+	 * Enum for determening what type the MyTypeHolder is.
+	 *
+	 */
 	public enum FieldType {
 		INTEGER, STRING, BOOL
 	}
 	
 
 	
+	/**
+	 * Constructor for setting integer
+	 * @param integer
+	 */
 	public MyTypeHolder(int integer) {
 		setInteger(integer);
 	}
 	
+	/**
+	 * Constuctor for setting String
+	 * @param string
+	 */
 	public MyTypeHolder(String string) {
 		setString(string);
 	}
 	
+	/**
+	 * Constructor for setting booleans
+	 * @param bool
+	 */
 	public MyTypeHolder(boolean bool) {
 		setBool(bool);
 	}
 	
+	/**
+	 * Setter for integer
+	 * Will also set the type of MyTypeHolder with an enum
+	 * @param integer
+	 */
 	public void setInteger(int integer) {
 		this.integer = integer;
 		setType(FieldType.INTEGER);
 	}
 	
+	/**
+	 * Setter for String
+	 * Will also set the type of MyTypeHolder with an enum
+	 * @param string
+	 */
 	public void setString(String string) {
 		this.string = string;
 		setType(FieldType.STRING);
 	}
 	
+	
+	/**
+	 * Setter for boolean
+	 * Will also set the type of MyTypeHolder with an enum
+	 * @param bool
+	 */
 	public void setBool(boolean bool) {
 		this.bool = bool;
 		setType(FieldType.BOOL);
@@ -52,11 +90,20 @@ public class MyTypeHolder {
 		return bool;
 	}
 	
+	
+	
+	/**
+	 * Sets the type of MyTypeHolder
+	 * @param type
+	 */
 	private void setType(FieldType type) {
 		this.type = type;
 
 	}
 	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString() {
 		String string;
