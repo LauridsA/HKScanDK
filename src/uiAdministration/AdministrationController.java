db73f57d5bf0ff6c8be308a6ac1e29e3cc3c6a19
mrurb <minikaliffen@gmail.com>
2016-12-08 13:06:09 +0100

    stuff

--- src/uiAdministration/AdministrationController.java
+++ src/uiAdministration/AdministrationController.java
@@ -11,35 +11,42 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 import javafx.util.Duration;
 
 public class AdministrationController {
 
-    @FXML
-    private Button createDailyMessageButton;
+
+
 
     @FXML
-    private ScrollPane dailyMessagePane;
+    private Button createProductuonStop;
 
     @FXML
-    private Label testLabel;
+    private VBox vboxList;
     
    
     public void initialize(){
-    	//createDailyMessageButton.setOnAction(e -> CreateDailyMessage(e));
+    	//createDailyMessageButton.setOnAction(e -> CreateDailyMessage(e));d
     	//testLabel.setText("test123");
     	ChangeLabel();
+    	//Label label = new Label("test456");
+    	//vboxList.getChildren().add(label);asd
     }
     
     private void ChangeLabel() {
-    	createDailyMessageButton.setOnAction(e->CreateDailyMessage(e));
+    	createProductuonStop.setOnAction(e->CreateDailyMessage(e));
+   
 
 	}
     
     
 	private void CreateDailyMessage(ActionEvent e) {
-		// TODO Auto-generated method stub
 		
+	 	for (int i = 0; i < 500; i++) {
+    		Label label = new Label("test" + i);
+			vboxList.getChildren().add(label);
+		}
 	}
 
 
