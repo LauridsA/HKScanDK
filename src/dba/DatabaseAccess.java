037757513230d4648e8909a5c6c7ddd8ed77e874
Laurids Andersen <lauridsandersen2013@gmail.com>
2016-12-09 10:56:10 +0100

    saved KappaRoss

--- src/dba/DatabaseAccess.java
+++ src/dba/DatabaseAccess.java
@@ -651,7 +651,7 @@ public class DatabaseAccess {
 	}
 
 	public int expectedPerHour(int teamId) {
-		String query = "DECLARE @team INT = ?; DECLARE @NightTeamAmount INT = (SELECT SUM(value) AS nightamount  FROM slaughteramount WHERE teamtimetableid = (SELECT TOP 1 teamnighttimetableid FROM teamtimetable JOIN batch ON teamtimetable.teamid = batch.teamdaytimetableid WHERE teamdaytimetableid = @team)); DECLARE @speed INT = (SELECT TOP 1 value FROM speed ORDER BY stimestamp DESC); SELECT (sum(value) - @NightTeamAmount)/@speed AS result FROM slaughteramount WHERE teamtimetableid = (SELECT TOP 1 teamnighttimetableid FROM batch WHERE teamnighttimetableid = @team OR teamdaytimetableid = @team) OR teamtimetableid = (SELECT TOP 1 teamdaytimetableid FROM batch WHERE teamnighttimetableid = @team OR teamdaytimetableid = @team);";
+		String query = "DECLARE @team INT = ?; DECLARE @NightTeamAmount INT = (SELECT SUM(value) AS nightamount FROM slaughteramount WHERE teamtimetableid = (SELECT TOP 1 teamnighttimetableid FROM teamtimetable JOIN batch ON teamtimetable.id = batch.teamdaytimetableid WHERE teamdaytimetableid = @team)); DECLARE @speed INT = (SELECT TOP 1 value FROM speed ORDER BY stimestamp DESC); SELECT (sum(value) - @NightTeamAmount)/@speed AS result FROM slaughteramount WHERE teamtimetableid = (SELECT TOP 1 teamnighttimetableid FROM batch WHERE teamnighttimetableid = @team OR teamdaytimetableid = @team) OR teamtimetableid = (SELECT TOP 1 teamdaytimetableid FROM batch WHERE teamnighttimetableid = @team OR teamdaytimetableid = @team);";
 		PreparedStatement statement = null;
 		ResultSet result = null;
 		Connection con = null;
