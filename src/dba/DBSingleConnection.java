f6b7a563364763d597357715623a31541fd2ae2a
Skyggfisk <mahasi93@hotmail.com>
2016-12-19 09:30:15 +0100

    Clean up aisle six.

--- src/dba/DBSingleConnection.java
+++ src/dba/DBSingleConnection.java
@@ -11,21 +11,24 @@ public class DBSingleConnection {
 	private static final String userName = "UCN_dmaa0216_2Sem_1";
 	private static final String passWord = "Password1!";
 	
+	@SuppressWarnings("unused")
 	private DatabaseMetaData dma;
 	private static Connection con;
 	private boolean inuse = false;
 	
 	
 	public DBSingleConnection() {
-		
+		// Empty constructor.
 	}
 	
+	/**
+	 * Private method used to establish connection the database.
+	 */
 	private void openConnection() {
 		String connectionString = "jdbc:sqlserver://" + server + ";databaseName=" + databaseName + ";user=" + userName + ";password=" + passWord;
 		
 		try {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-			//System.out.println("Driver find the driver");
 		} catch (Exception e) {
 			System.out.println("Driver not found");
 			e.printStackTrace();
@@ -36,7 +39,6 @@ public class DBSingleConnection {
 			con = DriverManager.getConnection(connectionString);
 			con.setAutoCommit(true);
 			dma = con.getMetaData();
-			//System.out.println("connection suceded");
 		} catch (Exception e) {
 			System.out.println("Con problem");
 			e.printStackTrace();
@@ -44,13 +46,14 @@ public class DBSingleConnection {
 
 	}
 	
-	
-	
+	/**
+	 * Used to close the current connection.<br>
+	 * Releases the connection for use by other threads.
+	 */
 	public synchronized void closeConnection() {
 		try {
 			con.close();
 			inuse = false;
-			//System.out.println("everyone wake up");
 			notifyAll();
 		} catch (Exception e) {
 			System.out.println("error");
@@ -58,8 +61,12 @@ public class DBSingleConnection {
 		}
 	}
 	
-	public synchronized Connection getDBcon()
-	{
+	/**
+	 * Synchronized method used to retrieve the current database connection session.<br>
+	 * If the current connection is in use it will call for the thread to wait.
+	 * @return Connection session.
+	 */
+	public synchronized Connection getDBcon() {
 		while (inuse) {
 			try {
 				System.out.println("I'm waiting");
