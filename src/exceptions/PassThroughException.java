de3c8f93679662572759cd366bbc882e8d347375
mrurb <minikaliffen@gmail.com>
2017-01-16 13:10:44 +0100

    pass t

---
+++ src/exceptions/PassThroughException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+@SuppressWarnings("serial")
+public class PassThroughException extends Exception{
+	public PassThroughException(String s, Throwable e) {
+		super(s, e);
+	}
+}
