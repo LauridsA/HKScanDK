10f50ea46370c1c1e5e8b022f842aa5b96e71d23
Daniel-PCBar <Daniel-PCBar@10.31.157.80>
2017-01-16 13:12:15 +0100

    fixed exception handling

--- src/controller/AdministrationController.java
+++ src/controller/AdministrationController.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 import dba.AdministrationDatabaseAccess;
 import dba.DBSingleConnection;
+import exceptions.DbaException;
 import model.DailyMessages;
 import model.ProductionStop;
 
@@ -26,8 +27,9 @@ public class AdministrationController {
 	 * @param timestamp system Unix time stamp, generated at call.
 	 * @param expire the expiration date for this message.
 	 * @param showDate the time this message should be displayed from.
+	 * @throws DbaException 
 	 */
-	public void createDailyMessage(String message, Long expire, Long showDate) {
+	public void createDailyMessage(String message, Long expire, Long showDate) throws DbaException {
 		Date time = new Date();
 		dba.createDailyMessage(message, time.getTime(), expire, showDate);
 	}
@@ -41,8 +43,9 @@ public class AdministrationController {
 	 * @param newTimeStamp new Unix time stamp to use
 	 * @param newExpire new expiration date
 	 * @param newShowDate new time to display the message
+	 * @throws DbaException 
 	 */
-	public void updateDailyMessage(int id, String newMessage, Long newTimeStamp, Long newExpire, Long newShowDate) {
+	public void updateDailyMessage(int id, String newMessage, Long newTimeStamp, Long newExpire, Long newShowDate) throws DbaException {
 		dba.updateDailyMessage(id, newMessage, newTimeStamp, newExpire, newShowDate);
 	}
 	
@@ -50,16 +53,18 @@ public class AdministrationController {
 	 * Used to delete a dailyMessage from the dailymessages table in database.
 	 * Uses id to find the dailymessage to delete.
 	 * @param id the id from UI used to find the dailyMessage in the dailymessages table.
+	 * @throws DbaException 
 	 */
-	public void deleteDailyMessage(int id) {
+	public void deleteDailyMessage(int id) throws DbaException {
 		dba.deleteDailyMessage(id);
 	}
 	
 	/**
 	 * Used to retrieve all DailyMessages from database.
 	 * @return all DailyMessages from the dailymessages table as ArrayList
+	 * @throws DbaException 
 	 */
-	public ArrayList<DailyMessages> getAllMessages() {
+	public ArrayList<DailyMessages> getAllMessages() throws DbaException {
 		return dba.getAllMessages();
 	}
 	
@@ -70,8 +75,9 @@ public class AdministrationController {
 	 * @param stopDescription String displaying a description of the stop.
 	 * @param TeamTimeTableId ID of timetable working at the time of stop.
 	 * @return 
+	 * @throws DbaException 
 	 */
-	public ProductionStop createStop(Long stopTime, int stopLength, String stopDescription, int teamTimeTableId) {
+	public ProductionStop createStop(Long stopTime, int stopLength, String stopDescription, int teamTimeTableId) throws DbaException {
 		return dba.createStop(stopTime, stopLength, stopDescription, teamTimeTableId);
 	}
 	
@@ -84,8 +90,9 @@ public class AdministrationController {
 	 * @param newStopLength new int for stopLength
 	 * @param newStopDescription new String to display
 	 * @param newTeamTimeTableId new int for teamTimeTable
+	 * @throws DbaException 
 	 */
-	public void updateStop(int id, Long newStopTime, int newStopLength, String newStopDescription, int newTeamTimeTableId) {
+	public void updateStop(int id, Long newStopTime, int newStopLength, String newStopDescription, int newTeamTimeTableId) throws DbaException {
 		dba.updateStop(id, newStopTime, newStopLength, newStopDescription, newTeamTimeTableId);
 	}
 	
@@ -93,16 +100,18 @@ public class AdministrationController {
 	 * Used to delete a ProductionStop from the productionstop table in database.
 	 * Uses id to find the productionStop to delete.
 	 * @param id the id from UI used to find the productionStop in the productionstop table.
+	 * @throws DbaException 
 	 */
-	public void deleteStop(int id) {
+	public void deleteStop(int id) throws DbaException {
 		dba.deleteStop(id);
 	}
 	
 	/**
 	 * Used to retrieve all ProductioStops from database.
 	 * @return all DailyMessages from the productionstop table as ArrayList
+	 * @throws DbaException 
 	 */
-	public ArrayList<ProductionStop> getAllStops() {
+	public ArrayList<ProductionStop> getAllStops() throws DbaException {
 		return dba.getAllStops();
 	}
 
