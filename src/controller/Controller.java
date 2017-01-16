a7242bd1f4719de26fb1da54222738c88a406c0c
mrurb <minikaliffen@gmail.com>
2017-01-16 13:15:43 +0100

    exception handling in controller

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -7,6 +7,8 @@ import java.util.Map;
 
 import dba.DBSingleConnection;
 import dba.DatabaseAccess;
+import exceptions.DbaException;
+import exceptions.PassThroughException;
 import model.FieldTypes;
 import model.MyTypeHolder;
 import model.ProductionStop;
@@ -27,8 +29,9 @@ public class Controller {
 	 * Primary method for retrieving values; Chooses the proper method.
 	 * @param fieldType
 	 * @return {@link MyTypeHolder}(Int/Bool/String)
+	 * @throws PassThroughException 
 	 */
-	public MyTypeHolder getValue(FieldTypes fieldType){
+	public MyTypeHolder getValue(FieldTypes fieldType) throws PassThroughException{
 		switch (fieldType) {
 		case SPEED:
 			return getSpeed();
@@ -68,63 +71,99 @@ public class Controller {
 	/**
 	 * Retrieves the speed from the DB and returns it as an int (average from the last hour)
 	 * @return the speed as MyTypeHolder(int) for the last hour from DB
+	 * @throws PassThroughException 
 	 */
-	public MyTypeHolder getSpeed() {
-		return new MyTypeHolder(dba.getCurrentSpeed());
+	public MyTypeHolder getSpeed() throws PassThroughException {
+		try {
+			return new MyTypeHolder(dba.getCurrentSpeed());
+		} catch (DbaException e) {
+			throw new PassThroughException(e.getMessage(), e);
+		}
 	}
 	
 	/**
 	 * Retrieves the average weight from the DB and returns it as an int 
 	 * @return the average weight on the current batch
+	 * @throws PassThroughException 
 	 */
-	public MyTypeHolder getAvgWeight() {
-		return new MyTypeHolder(dba.getAvgWeight());
+	public MyTypeHolder getAvgWeight() throws PassThroughException {
+		try {
+			return new MyTypeHolder(dba.getAvgWeight());
+		} catch (DbaException e) {
+			throw new PassThroughException(e.getMessage(), e);
+		}
 	}
 
 	/**
 	 * Retrieves the amount of chickens slaughtered by day team from DB
 	 * @return amount of chickens slaughtered as an int by day team
+	 * @throws PassThroughException 
 	 */
-	private MyTypeHolder getCurrentSlaughterAmountDay() {
+	private MyTypeHolder getCurrentSlaughterAmountDay() throws PassThroughException {
 		Date time = new Date();
-		return new MyTypeHolder(dba.getSlaughterAmountDay(time.getTime()));
+		try {
+			return new MyTypeHolder(dba.getSlaughterAmountDay(time.getTime()));
+		} catch (DbaException e) {
+			throw new PassThroughException(e.getMessage(), e);
+		}
 	}
 
 	/**
 	 * Retrieves the amount of chickens slaughtered by day night from DB
 	 * @return amount of chickens slaughtered as an int by night team
+	 * @throws PassThroughException 
 	 */
-	private MyTypeHolder getCurrentSlaughterAmountNight() {
+	private MyTypeHolder getCurrentSlaughterAmountNight() throws PassThroughException {
 		Date time = new Date();
-		return new MyTypeHolder(dba.getSlaughterAmountNight(time.getTime()));
+		try {
+			return new MyTypeHolder(dba.getSlaughterAmountNight(time.getTime()));
+		} catch (DbaException e) {
+			throw new PassThroughException(e.getMessage(), e);
+		}
 	}
 
 	/**
 	 * Updates the local machine to save who is the current working team (day or night, from team timetable)
 	 * @return null
+	 * @throws PassThroughException 
 	 */
 	@SuppressWarnings("static-access")
-	public MyTypeHolder getCurrentWorkingTeam() {
+	public MyTypeHolder getCurrentWorkingTeam() throws PassThroughException {
 		Date time = new Date();
-		dba.getCurrentWorkingTeam(time.getTime());
+		try {
+			dba.getCurrentWorkingTeam(time.getTime());
+		} catch (DbaException e) {
+			throw new PassThroughException(e.getMessage(), e);
+		}
 		return new MyTypeHolder(WorkingTeam.getInstance().getTeamTimeTableId());
 	}
 
 	/**
 	 * the result of getCurrentTotalSlaughterAmount-getCurrentSlaughterAmount from the night team as an int
 	 * @return the expected production for the day team as an int
+	 * @throws PassThroughException 
 	 */
-	private MyTypeHolder dayExpected() {
-		return new MyTypeHolder(dba.dayExpected(WorkingTeam.getInstance().getTeamId()));
+	private MyTypeHolder dayExpected() throws PassThroughException {
+		try {
+			return new MyTypeHolder(dba.dayExpected(WorkingTeam.getInstance().getTeamId()));
+		} catch (DbaException e) {
+			throw new PassThroughException(e.getMessage(), e);
+		}
 	}
 	
 	/**
 	 * Retrieves the result of dayExpected / Speed from the database as a single query
 	 * @return dayExpected / getSpeed as an Integer
+	 * @throws PassThroughException 
 	 */
-	private MyTypeHolder expectedPerHour() {
+	private MyTypeHolder expectedPerHour() throws PassThroughException {
 		Date time = new Date();
-		int eke = dba.expectedPerHour(WorkingTeam.getInstance().getTeamId(),time.getTime());
+		int eke;
+		try {
+			eke = dba.expectedPerHour(WorkingTeam.getInstance().getTeamId(),time.getTime());
+		} catch (DbaException e) {
+			throw new PassThroughException(e.getMessage(), e);
+		}
 		if(eke <= 0) {
 			eke = 0;
 		}
@@ -134,11 +173,17 @@ public class Controller {
 	/**
 	 * Calculates the expected end time for the day team based on remaining amounts and speed.
 	 * @return the expected end time for the day team as unix time
+	 * @throws PassThroughException 
 	 */
-	private MyTypeHolder expectedFinish() {
+	private MyTypeHolder expectedFinish() throws PassThroughException {
 		
 		int totalTime = 0;
-		Map<Integer, Integer> map = dba.expectedFinish(WorkingTeam.getInstance().getTeamId());
+		Map<Integer, Integer> map;
+		try {
+			map = dba.expectedFinish(WorkingTeam.getInstance().getTeamId());
+		} catch (DbaException e) {
+			throw new PassThroughException(e.getMessage(), e);
+		}
 		for (Map.Entry<Integer, Integer> entry : map.entrySet()){
 			if (entry.getKey() == 1) {
 				totalTime += entry.getValue() / 217;
@@ -155,27 +200,44 @@ public class Controller {
 	/**
 	 * retrieves the total slaughtered chickens for the working day as a single query
 	 * @return the current total slaughtered chickens for the working day <b>so far</b> as an int
+	 * @throws PassThroughException 
 	 */
-	public MyTypeHolder getTotalCurrentSlaughterAmount() {
-		int myresult = dba.getTotalCurrentSlaughterAmount(WorkingTeam.getInstance().getTeamId());
+	public MyTypeHolder getTotalCurrentSlaughterAmount() throws PassThroughException {
+		int myresult;
+		try {
+			myresult = dba.getTotalCurrentSlaughterAmount(WorkingTeam.getInstance().getTeamId());
+		} catch (DbaException e) {
+			throw new PassThroughException(e.getMessage(), e);
+		}
 		return new MyTypeHolder(myresult);
 	}
 	
 	/**
 	 * retrieves the currently non-expired production stop messages
 	 * @return the object ProductionStop
+	 * @throws PassThroughException 
 	 */
-	private MyTypeHolder getProductionStop() {
-		ArrayList<ProductionStop> result = dba.getAllStops();
+	private MyTypeHolder getProductionStop() throws PassThroughException {
+		ArrayList<ProductionStop> result;
+		try {
+			result = dba.getAllStops();
+		} catch (DbaException e) {
+			throw new PassThroughException(e.getMessage(), e);
+		}
 		return new MyTypeHolder(result);
 	}
 	
 	/**
 	 * Retrieves the total amount to be slaughtered for the working day.
 	 * @return total amount to be slaughtered for the day as an integer.
+	 * @throws PassThroughException 
 	 */
-	public MyTypeHolder getTotalSlaughterAmount() {
-		return new MyTypeHolder(dba.totalSlaughterAmount(WorkingTeam.getInstance().getTeamId()));
+	public MyTypeHolder getTotalSlaughterAmount() throws PassThroughException {
+		try {
+			return new MyTypeHolder(dba.totalSlaughterAmount(WorkingTeam.getInstance().getTeamId()));
+		} catch (DbaException e) {
+			throw new PassThroughException(e.getMessage(), e);
+		}
 	}
 	
 	/**
@@ -190,45 +252,72 @@ public class Controller {
 	/**
 	 * Retrieves the amount of production stops for the working day, along with their stop time for the day team.
 	 * @return amount of production stop and their respective stop time.
+	 * @throws PassThroughException 
 	 */
-	private MyTypeHolder getNoStopDay() {
+	private MyTypeHolder getNoStopDay() throws PassThroughException {
 		Date time = new Date();
-		int myresult = dba.getNoStopDay(time.getTime());
+		int myresult;
+		try {
+			myresult = dba.getNoStopDay(time.getTime());
+		} catch (DbaException e) {
+			throw new PassThroughException(e.getMessage(), e);
+		}
 		return new MyTypeHolder(myresult);
 	}
 	
 	/**
 	 * Retrieves the amount of production stops for the working day, along with their stop time for the night team.
 	 * @return amount of production stop and their respective stop time.
+	 * @throws PassThroughException 
 	 */
-	private MyTypeHolder getNoStopNight() {
+	private MyTypeHolder getNoStopNight() throws PassThroughException {
 		Date time = new Date();
-		int myresult = dba.getNoStopNight(time.getTime());
+		int myresult;
+		try {
+			myresult = dba.getNoStopNight(time.getTime());
+		} catch (DbaException e) {
+			throw new PassThroughException(e.getMessage(), e);
+		}
 		return new MyTypeHolder(myresult);
 	}
 	
 	/**
 	 * Retrieves all batches for the working day and evaluates whether any of them is organic.
 	 * @return returns true, if any of the batches for the working day is organic.
+	 * @throws PassThroughException 
 	 */
-	public MyTypeHolder getOrganic() {
-		return new MyTypeHolder(dba.getOrganic(WorkingTeam.getInstance().getTeamId()));
+	public MyTypeHolder getOrganic() throws PassThroughException {
+		try {
+			return new MyTypeHolder(dba.getOrganic(WorkingTeam.getInstance().getTeamId()));
+		} catch (DbaException e) {
+			throw new PassThroughException(e.getMessage(), e);
+		}
 	}
 	
 	/**
 	 * @param type The type of the refresh rate to be retrieved (From {@link FieldTypes}).
 	 * @return returns the refresh rate of the given type, 10 if it isn't found.
+	 * @throws PassThroughException 
 	 */
-	public int getRefreshRate(FieldTypes type) {
-		return dba.getRefreshRate(type);
+	public int getRefreshRate(FieldTypes type) throws PassThroughException {
+		try {
+			return dba.getRefreshRate(type);
+		} catch (DbaException e) {
+			throw new PassThroughException(e.getMessage(), e);
+		}
 	}
 
 	/**
 	 * Retrieves an integer representing the total amount of production stops.
 	 * @return total amount of stops
+	 * @throws PassThroughException 
 	 */
-	public int getTotalStops() {
-		return dba.getTotalStops();
+	public int getTotalStops() throws PassThroughException {
+		try {
+			return dba.getTotalStops();
+		} catch (DbaException e) {
+			throw new PassThroughException(e.getMessage(), e);
+		}
 	}
 	
 	/**
